a08c71e6816f67ec6ab27c613c6e458e761ff8d5
alvinAdler <alvinadlersampow@gmail.com>
2021-11-22 17:13:23 +0700

    Added keyboard visualization in the drill mode

--- client/src/components/PracticePage/DrillModePage/DrillModePage.jsx
+++ client/src/components/PracticePage/DrillModePage/DrillModePage.jsx
@@ -6,6 +6,7 @@ import './DrillModePage_master.css'
 
 import useKey from '../../Utilities/useKey'
 import fingersMappingData from '../../Utilities/fingersMappingData'
+import keysArrary from '../../Utilities/keysArray'
 
 const DrillModePage = () => {
 
@@ -89,78 +90,17 @@ const DrillModePage = () => {
                 <span>{(currentLetter.current !== "" && currentLetter.current !== undefined) && fingersMappingData[currentLetter.current.toLowerCase()].text}</span>
             </div>
             <div className="keyboard-wrapper">
-                <div className="keyboard-row number-row">
-                    <span className="key size-sm">`</span>
-                    <span className="key size-sm">1</span>
-                    <span className="key size-sm">2</span>
-                    <span className="key size-sm">3</span>
-                    <span className="key size-sm">4</span>
-                    <span className="key size-sm">5</span>
-                    <span className="key size-sm">6</span>
-                    <span className="key size-sm">7</span>
-                    <span className="key size-sm">8</span>
-                    <span className="key size-sm">9</span>
-                    <span className="key size-sm">0</span>
-                    <span className="key size-sm">-</span>
-                    <span className="key size-sm">=</span>
-                    <span className="key">Backspace</span>
-                </div>
-                <div className="keyboard-row top-row">
-                    <span className="key size-md">Tab</span>
-                    <span className="key size-sm">q</span>
-                    <span className="key size-sm">w</span>
-                    <span className="key size-sm">e</span>
-                    <span className="key size-sm">r</span>
-                    <span className="key size-sm">t</span>
-                    <span className="key size-sm">y</span>
-                    <span className="key size-sm">u</span>
-                    <span className="key size-sm">i</span>
-                    <span className="key size-sm">o</span>
-                    <span className="key size-sm">p</span>
-                    <span className="key size-sm">[</span>
-                    <span className="key size-sm">]</span>
-                    <span className="key size-sm">\</span>
-                </div>
-                <div className="keyboard-row home-row">
-                    <span className="key size-lg">CAPS</span>
-                    <span className="key size-sm">a</span>
-                    <span className="key size-sm">s</span>
-                    <span className="key size-sm">d</span>
-                    <span className="key size-sm">f</span>
-                    <span className="key size-sm">g</span>
-                    <span className="key size-sm">h</span>
-                    <span className="key size-sm">j</span>
-                    <span className="key size-sm">k</span>
-                    <span className="key size-sm">l</span>
-                    <span className="key size-sm">;</span>
-                    <span className="key size-sm">'</span>
-                    <span className="key size-lg">Ent</span>
-                </div>
-                <div className="keyboard-row bottom-row">
-                    <span className="key size-exlg">Shift</span>
-                    <span className="key size-sm">z</span>
-                    <span className="key size-sm">x</span>
-                    <span className="key size-sm">c</span>
-                    <span className="key size-sm">v</span>
-                    <span className="key size-sm">b</span>
-                    <span className="key size-sm">n</span>
-                    <span className="key size-sm">m</span>
-                    <span className="key size-sm">,</span>
-                    <span className="key size-sm">.</span>
-                    <span className="key size-sm">/</span>
-                    <span className="key size-exlg">Shift</span>
-                </div>
-                <div className="keyboard-row utility-row">
-                    <span className="key size-util">Ctrl</span>
-                    <span className="key size-util">Fn</span>
-                    <span className="key size-util">Win</span>
-                    <span className="key size-util">Alt</span>
-                    <span className="key" style={{flexGrow: 5}}>SPACE</span>
-                    <span className="key size-util">Alt</span>
-                    <span className="key size-util">Fn</span>
-                    <span className="key size-util">Win</span>
-                    <span className="key size-util">Ctrl</span>
-                </div>
+                {Array.from(new Array(5)).map((item, indexOuter) => {
+                    return(
+                        <div key={indexOuter} className="keyboard-row utility-row">
+                            {keysArrary[indexOuter].map((item, indexInner) => {
+                                return(
+                                    <span key={indexInner} className={`${fingersMappingData[item].keyClassName} ${item === currentLetter.current && "key-image-current"}`}>{item}</span>
+                                )
+                            })}
+                        </div>
+                    )
+                })}
             </div>
         </div>
     )
--- client/src/components/Utilities/fingersMappingData.js
+++ client/src/components/Utilities/fingersMappingData.js
@@ -8,172 +8,233 @@ const RF = "Ring Finger"
 const LF = "Little Finger"
 
 const fingersMappingData = {
-    "q": {
+    "`": {
         text: `${LH}, ${LF}`,
-        graphic: ""
-    },
-    "w": {
-        text: `${LH}, ${RF}`,
-        graphic: ""
-    },
-    "e": {
-        text: `${LH}, ${MF}`,
-        graphic: ""
-    },
-    "r": {
-        text: `${LH}, ${IF}`,
-        graphic: ""
-    },
-    "t": {
-        text: `${LH}, ${IF}`,
-        graphic: ""
-    },
-    "y": {
-        text: `${RH}, ${IF}`,
-        graphic: ""
-    },
-    "u": {
-        text: `${RH}, ${IF}`,
-        graphic: ""
-    },
-    "i": {
-        text: `${RH}, ${MF}`,
-        graphic: ""
-    },
-    "o": {
-        text: `${RH}, ${RF}`,
-        graphic: ""
-    },
-    "p": {
-        text: `${RH}, ${LF}`,
-        graphic: ""
-    },
-    "a": {
-        text: `${LH}, ${LF}`,
-        graphic: ""
-    },
-    "s": {
-        text: `${LH}, ${RF}`,
-        graphic: ""
-    },
-    "d": {
-        text: `${LH}, ${MF}`,
-        graphic: ""
-    },
-    "f": {
-        text: `${LH}, ${IF}`,
-        graphic: ""
-    },
-    "g": {
-        text: `${LH}, ${IF}`,
-        graphic: ""
-    },
-    "h": {
-        text: `${RH}, ${IF}`,
-        graphic: ""
-    },
-    "j": {
-        text: `${RH}, ${IF}`,
-        graphic: ""
-    },
-    "k": {
-        text: `${RH}, ${MF}`,
-        graphic: ""
-    },
-    "l": {
-        text: `${RH}, ${RF}`,
-        graphic: ""
-    },
-    ";": {
-        text: `${RH}, ${LF}`,
-        graphic: ""
-    },
-    "'": {
-        text: `${RH}, ${LF}`,
-        graphic: ""
-    },
-    "z": {
-        text: `${LH}, ${LF}`,
-        graphic: ""
-    },
-    "x": {
-        text: `${LH}, ${RF}`,
-        graphic: ""
-    },
-    "c": {
-        text: `${LH}, ${MF}`,
-        graphic: ""
-    },
-    "v": {
-        text: `${LH}, ${IF}`,
-        graphic: ""
-    },
-    "b": {
-        text: `${LH}, ${IF}`,
-        graphic: ""
-    },
-    "n": {
-        text: `${RH}, ${IF}`,
-        graphic: ""
-    },
-    "m": {
-        text: `${RH}, ${IF}`,
-        graphic: ""
-    },
-    ",": {
-        text: `${RH}, ${MF}`,
-        graphic: ""
-    },
-    ".": {
-        text: `${RH}, ${RF}`,
-        graphic: ""
-    },
-    "/": {
-        text: `${RH}, ${LF}`,
-        graphic: ""
+        keyClassName: "key size-sm little-finger-key"
     },
     "1": {
         text: `${LH}, ${LF}`,
-        graphic: ""
+        keyClassName: "key size-sm little-finger-key"
     },
     "2": {
         text: `${LH}, ${RF}`,
-        graphic: ""
+        keyClassName: "key size-sm ring-finger-key"
     },
     "3": {
         text: `${LH}, ${MF}`,
-        graphic: ""
+        keyClassName: "key size-sm middle-finger-key"
     },
     "4": {
         text: `${LH}, ${IF}`,
-        graphic: ""
+        keyClassName: "key size-sm index-left-key"
     },
     "5": {
         text: `${LH}, ${IF}`,
-        graphic: ""
+        keyClassName: "key size-sm index-left-key"
     },
     "6": {
         text: `${RH}, ${IF}`,
-        graphic: ""
+        keyClassName: "key size-sm index-right-key"
     },
     "7": {
         text: `${RH}, ${IF}`,
-        graphic: ""
+        keyClassName: "key size-sm index-right-key"
     },
     "8": {
         text: `${RH}, ${MF}`,
-        graphic: ""
+        keyClassName: "key size-sm middle-finger-key"
     },
     "9": {
         text: `${RH}, ${RF}`,
-        graphic: ""
+        keyClassName: "key size-sm ring-finger-key"
     },
     "0": {
         text: `${RH}, ${LF}`,
-        graphic: ""
+        keyClassName: "key size-sm little-finger-key"
+    },
+    "-": {
+        text: `${RH}, ${LF}`,
+        keyClassName: "key size-sm little-finger-key"
+    },
+    "=": {
+        text: `${RH}, ${LF}`,
+        keyClassName: "key size-sm little-finger-key"
+    },
+    "Backspace": {
+        text: `${RH}, ${LF}`,
+        keyClassName: "key little-finger-key"
+    },
+    "Tab": {
+        text: `${LH}, ${LF}`,
+        keyClassName: "key size-md little-finger-key"
+    },
+    "q": {
+        text: `${LH}, ${LF}`,
+        keyClassName: "key size-sm little-finger-key"
+    },
+    "w": {
+        text: `${LH}, ${RF}`,
+        keyClassName: "key size-sm ring-finger-key"
+    },
+    "e": {
+        text: `${LH}, ${MF}`,
+        keyClassName: "key size-sm middle-finger-key"
+    },
+    "r": {
+        text: `${LH}, ${IF}`,
+        keyClassName: "key size-sm index-left-key"
+    },
+    "t": {
+        text: `${LH}, ${IF}`,
+        keyClassName: "key size-sm index-left-key"
+    },
+    "y": {
+        text: `${RH}, ${IF}`,
+        keyClassName: "key size-sm index-right-key"
+    },
+    "u": {
+        text: `${RH}, ${IF}`,
+        keyClassName: "key size-sm index-right-key"
+    },
+    "i": {
+        text: `${RH}, ${MF}`,
+        keyClassName: "key size-sm middle-finger-key"
+    },
+    "o": {
+        text: `${RH}, ${RF}`,
+        keyClassName: "key size-sm ring-finger-key"
+    },
+    "p": {
+        text: `${RH}, ${LF}`,
+        keyClassName: "key size-sm little-finger-key"
+    },
+    "[": {
+        text: `${RH}, ${LF}`,
+        keyClassName: "key size-sm little-finger-key"
+    },
+    "]": {
+        text: `${RH}, ${LF}`,
+        keyClassName: "key size-sm little-finger-key"
+    },
+    "\\": {
+        text: `${RH}, ${LF}`,
+        keyClassName: "key size-sm little-finger-key"
+    },
+    "CAPS": {
+        text: `${LH}, ${LF}`,
+        keyClassName: "key size-lg little-finger-key"
+    },
+    "a": {
+        text: `${LH}, ${LF}`,
+        keyClassName: "key size-sm little-finger-key"
+    },
+    "s": {
+        text: `${LH}, ${RF}`,
+        keyClassName: "key size-sm ring-finger-key"
+    },
+    "d": {
+        text: `${LH}, ${MF}`,
+        keyClassName: "key size-sm middle-finger-key"
+    },
+    "f": {
+        text: `${LH}, ${IF}`,
+        keyClassName: "key size-sm index-left-key"
+    },
+    "g": {
+        text: `${LH}, ${IF}`,
+        keyClassName: "key size-sm index-left-key"
+    },
+    "h": {
+        text: `${RH}, ${IF}`,
+        keyClassName: "key size-sm index-right-key"
+    },
+    "j": {
+        text: `${RH}, ${IF}`,
+        keyClassName: "key size-sm index-right-key"
+    },
+    "k": {
+        text: `${RH}, ${MF}`,
+        keyClassName: "key size-sm middle-finger-key"
+    },
+    "l": {
+        text: `${RH}, ${RF}`,
+        keyClassName: "key size-sm ring-finger-key"
+    },
+    ";": {
+        text: `${RH}, ${LF}`,
+        keyClassName: "key size-sm little-finger-key"
+    },
+    "'": {
+        text: `${RH}, ${LF}`,
+        keyClassName: "key size-sm little-finger-key"
+    },
+    "Ent": {
+        text: `${RH}, ${LF}`,
+        keyClassName: "key size-lg little-finger-key"
+    },
+    "z": {
+        text: `${LH}, ${LF}`,
+        keyClassName: "key size-sm little-finger-key"
+    },
+    "x": {
+        text: `${LH}, ${RF}`,
+        keyClassName: "key size-sm ring-finger-key"
+    },
+    "c": {
+        text: `${LH}, ${MF}`,
+        keyClassName: "key size-sm middle-finger-key"
+    },
+    "v": {
+        text: `${LH}, ${IF}`,
+        keyClassName: "key size-sm index-left-key"
+    },
+    "b": {
+        text: `${LH}, ${IF}`,
+        keyClassName: "key size-sm index-left-key"
+    },
+    "n": {
+        text: `${RH}, ${IF}`,
+        keyClassName: "key size-sm index-right-key"
+    },
+    "m": {
+        text: `${RH}, ${IF}`,
+        keyClassName: "key size-sm index-right-key"
+    },
+    ",": {
+        text: `${RH}, ${MF}`,
+        keyClassName: "key size-sm middle-finger-key"
+    },
+    ".": {
+        text: `${RH}, ${RF}`,
+        keyClassName: "key size-sm ring-finger-key"
+    },
+    "/": {
+        text: `${RH}, ${LF}`,
+        keyClassName: "key size-sm little-finger-key"
+    },
+    "Shift": {
+        text: `${LH}/${RH}, ${LF}`,
+        keyClassName: "key size-exlg little-finger-key"
     },
     " ": {
-        text: `${LH}/${RH}, ${TF}`
+        text: `${LH}/${RH}, ${TF}`,
+        keyClassName: "key space util-key"
+    },
+    "Ctrl": {
+        text: ``,
+        keyClassName: "key size-util util-key"
+    },
+    "Fn": {
+        text: ``,
+        keyClassName: "key size-util util-key"
+    },
+    "Win": {
+        text: ``,
+        keyClassName: "key size-util util-key"
+    },
+    "Alt": {
+        text: ``,
+        keyClassName: "key size-util util-key"
     }
 }
 
