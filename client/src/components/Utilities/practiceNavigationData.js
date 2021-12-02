f127d2e832205a1e5823044f08e760bbf9e1be74
alvinAdler <alvinadlersampow@gmail.com>
2021-12-03 02:29:45 +0700

    Updated the visualization of selection in the practice page

--- client/src/components/Utilities/practiceNavigationData.js
+++ client/src/components/Utilities/practiceNavigationData.js
@@ -2,41 +2,41 @@ const practiceNavigationData = {
     "drillMode": [
         {
             name: "homeRow",
-            description: "This is a simple description for the drill mode"
+            description: "This practice consists of the following characters: a s d f g h j k l ;  '"
         },
         {
             name: "topRow",
-            description: "This is a simple description for the drill mode"
+            description: "This practice consists of the following characters: q w e r t y u i o p [ ] \\"
         },
         {
             name: "bottomRow",
-            description: "This is a simple description for the drill mode"
+            description: "This practice consists of the following characters: z x c v b n m , . /"
         },
         {
             name: "numberRow",
-            description: "This is a simple description for the drill mode"
+            description: "This practice consists of the following characters: 1 2 3 4 5 6 7 8 9 0 - ="
         },
         {
             name: "allKeys",
-            description: "This is a simple description for the drill mode"
+            description: "This practice consists of the combination from all the characters above"
         },
         {
             name: "commonWords",
-            description: "This is a simple description for the drill mode"
+            description: "This practice serves 50 out of 1000 of the most common words in English"
         },
     ],
     "gameMode": [
         {
             name: "easy",
-            description: "This is a simple description for the game mode"
+            description: "This mode has the slowest alien drop and spawn rate. Suitable for the beginners."
         },
         {
             name: "medium",
-            description: "This is a simple description for the game mode"
+            description: "This mode has slightly faster alien drop and spawn rate. Suitable for the intermediate typist."
         },
         {
             name: "hard",
-            description: "This is a simple description for the game mode"
+            description: "This mode has the faster alien drop and spawn rate. Recommended for the one that has enough skills in touch typing."
         },
         
     ]
