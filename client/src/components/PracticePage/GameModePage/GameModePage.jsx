9998ae9a29c6720e7f97467ab259ba440a76ff87
alvinAdler <alvinadlersampow@gmail.com>
2021-12-13 04:39:19 +0700

    Minor changes

--- client/src/components/PracticePage/GameModePage/GameModePage.jsx
+++ client/src/components/PracticePage/GameModePage/GameModePage.jsx
@@ -18,7 +18,7 @@ import { randomInteger, markLastVisitedPath, getUserCookie, capitalizeString } f
 const FRAME_TRANS_LIMIT = 5
 const FRAME_PER_SECOND = 60
 
-const DEVELOPER_MODE = false
+const DEVELOPER_MODE = true
 
 const SCORE_HIGH = 300
 const SCORE_MID = 200
@@ -393,8 +393,7 @@ const startAnimation = () => {
             },
             mainSheets["alienSpriteSheet"], alienData,
             {
-                posX: mainCanvas.current.width - 100,
-                // posX: randomInteger(0, mainCanvas.current.width - alienData.screenWidth),
+                posX: randomInteger(0, mainCanvas.current.width - alienData.screenWidth),
                 posY: -90
             },
             {
--- client/src/components/Utilities/classes/Tank.js
+++ client/src/components/Utilities/classes/Tank.js
@@ -2,8 +2,8 @@ import Sprite2 from "./Sprite2";
 import CannonBall from './CannonBall.js'
 
 const CANNONBALL_VELOCITY = {
-    x: 20,
-    y: 20
+    x: 5,
+    y: 5
 }
 
 
